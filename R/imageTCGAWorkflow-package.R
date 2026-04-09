0a4d254615566d23d5459b559e8127247b8900b2
billila <ilaria.billato@studenti.unipd.it>
2026-04-09 18:31:23 +0200

    imageTCGAWorkflow create

---
+++ R/imageTCGAWorkflow-package.R
@@ -0,0 +1,10 @@
+#' imageTCGAWorkflow: Workflow for Histopathology Image Analysis with TCGA Data
+#'
+#' A Bioconductor workflow demonstrating end-to-end histopathology image
+#' analysis using TCGA diagnostic whole-slide images. See the package vignette
+#' for the complete workflow.
+#'
+#' @seealso \code{vignette("imageTCGAWorkflow", package = "imageTCGAWorkflow")}
+#'
+#' @keywords internal
+"_PACKAGE"
